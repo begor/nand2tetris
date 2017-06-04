589ae43827130a9c813fcbb22cd73929d9ffd932
Biryukov Egor <birykovegor@gmail.com>
2017-06-04 15:43:03 +0300

    Mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R1
+D=M
+
+@n
+M=D
+
+(LOOP)
+    @n
+    D=M
+    @END
+    D;JEQ
+
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    @n
+    M=M-1
+
+    @LOOP
+    0;JEQ
+
+(END)
+    @END
+    0;JEQ
